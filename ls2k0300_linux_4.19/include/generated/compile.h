e2d3f77cd8f3762dca6721513ca50830ed1dfcdd
xiaom <47885808@qq.com>
2025-03-07 11:19:31 +0800

    添加交叉编译工具和opencv4.10库

--- ls2k0300_linux_4.19/include/generated/compile.h
+++ ls2k0300_linux_4.19/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 199 */
+/* This file is auto generated, version 200 */
 /* SMP */
 #define UTS_MACHINE "loongarch64"
-#define UTS_VERSION "#199 SMP Thu Mar 6 18:32:47 CST 2025"
+#define UTS_VERSION "#200 SMP Fri Mar 7 11:16:01 CST 2025"
 #define LINUX_COMPILE_BY "xiaom"
 #define LINUX_COMPILE_HOST "ubuntu24.04"
 #define LINUX_COMPILER "gcc version 8.3.0 (LoongArch GNU toolchain rc1.6 (20241115))"

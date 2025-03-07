d248911d727526ac75adae328ce975e5cc8415ed
xiaom <47885808@qq.com>
2025-03-07 21:06:42 +0800

    update

--- ls2k0300_linux_4.19/build.sh
+++ ls2k0300_linux_4.19/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 echo "执行自动编译+scp传输"
 
-export PATH=/home/xiaom/loongson_2k0300/toolchain/loongson-gnu-toolchain-8.3-x86_64-loongarch64-linux-gnu-rc1.6/bin:$PATH
+export PATH=/opt/ls_2k0300_env/loongson-gnu-toolchain-8.3-x86_64-loongarch64-linux-gnu-rc1.6/bin:$PATH
 
 make -j12
 

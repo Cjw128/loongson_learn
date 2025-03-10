526c1e96876f7f8c19b7d350ec27227885242dfa
xiaom <47885808@qq.com>
2025-03-10 19:21:18 +0800

    提交WIFI驱动

--- ls2k0300_linux_4.19/include/generated/compile.h
+++ ls2k0300_linux_4.19/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 213 */
+/* This file is auto generated, version 214 */
 /* SMP */
 #define UTS_MACHINE "loongarch64"
-#define UTS_VERSION "#213 SMP Mon Mar 10 18:54:39 CST 2025"
+#define UTS_VERSION "#214 SMP Mon Mar 10 19:05:24 CST 2025"
 #define LINUX_COMPILE_BY "xiaom"
 #define LINUX_COMPILE_HOST "ubuntu24.04"
 #define LINUX_COMPILER "gcc version 8.3.0 (LoongArch GNU toolchain rc1.6 (20241115))"

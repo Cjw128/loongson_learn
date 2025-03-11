8fea42337a746b4363e0b6653dfde595cde8799e
xiaom <47885808@qq.com>
2025-03-11 10:40:22 +0800

    提交pdf

--- ls2k0300_linux_4.19/include/generated/compile.h
+++ ls2k0300_linux_4.19/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 214 */
+/* This file is auto generated, version 215 */
 /* SMP */
 #define UTS_MACHINE "loongarch64"
-#define UTS_VERSION "#214 SMP Mon Mar 10 19:05:24 CST 2025"
+#define UTS_VERSION "#215 SMP Tue Mar 11 10:38:50 CST 2025"
 #define LINUX_COMPILE_BY "xiaom"
 #define LINUX_COMPILE_HOST "ubuntu24.04"
 #define LINUX_COMPILER "gcc version 8.3.0 (LoongArch GNU toolchain rc1.6 (20241115))"

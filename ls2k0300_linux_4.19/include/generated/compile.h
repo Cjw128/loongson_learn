fe79025e1bb5b501a9cf61516d2936c0e78f09e2
Cjw128 <598949340@qq.com>
2025-10-01 13:27:05 +0800

    update gitignore

--- ls2k0300_linux_4.19/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 231 */
-/* SMP */
-#define UTS_MACHINE "loongarch64"
-#define UTS_VERSION "#231 SMP Fri Sep 12 16:23:28 CST 2025"
-#define LINUX_COMPILE_BY "cjw"
-#define LINUX_COMPILE_HOST "cjw-virtual-machine"
-#define LINUX_COMPILER "gcc version 8.3.0 (LoongArch GNU toolchain rc1.6 (20241115))"
